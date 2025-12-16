ad209cf900fe516db02b686755e7edb37bb113d0
yairzawi770 <148704993+yairzawi770@users.noreply.github.com>
2025-12-16 10:45:57 +0200

    .

--- core/game_logic.py
+++ core/game_logic.py
@@ -1,12 +1,14 @@
-from deck import *
-def calculate_hand_value(hand: list[dict]) -> int:
-    total = 4
-    return total
+from deck import build_standard_deck
 
+def calculate_hand_value(hand: list[dict]) -> int:
+    if
+print(calculate_hand_value(build_standard_deck()))
 
 def deal_two_each(deck: list[dict], player: dict, dealer: dict) -> None:
-   player =  shuffle_by_suit(deck) * 2
-   dealer = shuffle_by_suit(deck) * 2
+    player["hand"] = range(0,51)
+    dealer["hand"] = range(0,51)
+print(deal_two_each())
+
 
 
 def dealer_play(deck: list[dict], dealer: dict) -> bool:
